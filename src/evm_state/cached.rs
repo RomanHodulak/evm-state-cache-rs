b6092eef191d38309b5c8b06b25b4ea3ba9c59c3
Roman Hodulák <hodulakr@gmail.com>
2024-05-30 01:32:40 +0200

    test(evm_state): Add unit tests for cached repository

--- src/evm_state/cached.rs
+++ src/evm_state/cached.rs
@@ -27,3 +27,89 @@ impl<InnerRepository: EvmStateRepository, C: Cache<Address, Account>> EvmStateRe
         self.cache.write(address, account);
     }
 }
+
+impl<InnerRepository: EvmStateRepository, C: Cache<Address, Account>>
+    CachedEvmStateRepository<InnerRepository, C>
+{
+    pub fn new(repository: InnerRepository, cache: C) -> Self {
+        Self {
+            inner: repository,
+            cache,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::InMemoryEvmStateRepository;
+    use primitive_types::U256;
+
+    struct DummyCache(Account);
+
+    impl Cache<Address, Account> for DummyCache {
+        fn read(&mut self, _key: &Address) -> Option<Account> {
+            Some(self.0.clone())
+        }
+
+        fn write(&mut self, _key: Address, _value: Account) {}
+    }
+
+    struct EmptyCache(Option<Account>);
+
+    impl Cache<Address, Account> for EmptyCache {
+        fn read(&mut self, _key: &Address) -> Option<Account> {
+            self.0.clone()
+        }
+
+        fn write(&mut self, _key: Address, value: Account) {
+            self.0.replace(value);
+        }
+    }
+
+    struct NoopEvmRepository;
+
+    impl EvmStateRepository for NoopEvmRepository {
+        fn get(&mut self, _address: &Address) -> Option<Account> {
+            None
+        }
+
+        fn replace(&mut self, _address: Address, _account: Account) {}
+    }
+
+    #[test]
+    fn test_account_is_primarily_taken_from_cache() {
+        let expected_account = Account::new(4, U256::zero(), U256::zero(), U256::zero());
+        let repository = NoopEvmRepository;
+        let cache = DummyCache(expected_account.clone());
+        let mut repository = CachedEvmStateRepository::new(repository, cache);
+
+        let actual_account = repository.get(&[0u8; 20]);
+
+        assert!(actual_account.is_some(), "Account not hit in cache");
+
+        let actual_account = actual_account.unwrap();
+
+        assert_eq!(expected_account, actual_account);
+    }
+
+    #[test]
+    fn test_account_is_loaded_from_repository_when_cache_misses() {
+        let expected_account = Account::new(4, U256::zero(), U256::zero(), U256::zero());
+        let mut repository = InMemoryEvmStateRepository::new();
+        repository.replace([0u8; 20], expected_account.clone());
+        let cache = EmptyCache(None);
+        let mut repository = CachedEvmStateRepository::new(repository, cache);
+
+        let actual_account = repository.get(&[0u8; 20]);
+
+        assert!(
+            actual_account.is_some(),
+            "Account not found using repository"
+        );
+
+        let actual_account = actual_account.unwrap();
+
+        assert_eq!(expected_account, actual_account);
+    }
+}
